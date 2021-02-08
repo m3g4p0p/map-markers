3716238815be4d5932b458471e9e8faf06cef642
Sebastian Seitz <bassistsagainstracists@gmx.net>
2021-02-08 16:24:52 +0100

    Updates

--- src/util.ts
+++
@@ -1,7 +0,0 @@
-export function filterObject<T = any>
-  (source: object, predicate: (key: string, value: T) => boolean) {
-  return Object.entries(source).reduce((result, [key, value]) => ({
-    ...result,
-    ...predicate(key, value) ? { [key]: value } : {}
-  }), {})
-}
